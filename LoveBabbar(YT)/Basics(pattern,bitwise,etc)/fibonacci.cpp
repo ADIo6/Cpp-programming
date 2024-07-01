84e3fd96df120337a704b68187fd040a8da2d03f
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-07-01 08:02:58 +0530

    Fibonacci Series

---
+++ LoveBabbar(YT)/Basics(pattern,bitwise,etc)/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=0,b= 1,n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    cout<<a<<" "<<b<<" ";
+
+    for (int i = 1; i <=n; i++)
+    {
+        int nextn=0;
+        cout<<(nextn=a+b)<<" ";
+        a=b;
+        b=nextn;
+    }
+    
+
+}
