fd5e05c06f037c3013f0bdfb8155ecb7567940ab
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-07-01 08:02:19 +0530

    getting to know about switch statements

---
+++ LoveBabbar(YT)/Basics(pattern,bitwise,etc)/switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+
+    while(1)
+    {
+        switch (num)
+        {
+        case 1: cout<<"no. 1"<<endl;
+            /* code */
+
+        exit(0);
+        default:cout<<"its default"<<endl;
+
+
+            exit(0);
+        }
+        
+    }
+}
